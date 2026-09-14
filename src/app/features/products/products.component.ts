2999cb80785cad208e202845e8ff44a75998186f
juancarlos-197 <juancarlos-197@unimayor.edu.co>
2026-09-14 15:38:02 -0500

    first commit

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -2,6 +2,7 @@ import { CurrencyPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from '../../core/products/product.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-products',
@@ -11,6 +12,23 @@ import { ProductService } from '../../core/products/product.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductsComponent {
+  readonly test = signal('');
+  private http = inject(HttpClient);
+
+  hacerTest() {
+    this.http.get<{ message: string }>('/api/test').subscribe({
+      next: (data) => {
+        console.log('Mensaje de prueba:', data.message);
+        this.test.set(data.message);
+      },
+      error: (err) => {
+        console.error('Error:', err);
+        this.test.set('Error al obtener el mensaje de prueba');
+      }
+    });
+
+  }
+
   private readonly productService = inject(ProductService);
   readonly query = signal('');
   readonly category = signal('Todos');
